442706f56e87941a5ab4528491a5dc4aa894d53e
RizqiMaulana14 <maulanafitrah14@gmail.com>
2026-04-16 00:18:27 +0700

    Revitalisasi Koneksi

--- BeasiswaDesktop/Login.cs
+++ BeasiswaDesktop/Login.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Data.SqlClient;
 using System.Windows.Forms;
 
@@ -6,13 +6,9 @@ namespace BeasiswaDesktop
 {
     public partial class Login : Form
     {
-        private readonly SqlConnection conn;
-        private readonly string connectionString =
-            "Data Source=LAPTOP-QEMA84FU\\HOME;Initial Catalog=beasiswaDB;Integrated Security=True";
         public Login()
         {
             InitializeComponent();
-            conn = new SqlConnection(connectionString);
         }
 
         private void Login_Button(object sender, EventArgs e)
@@ -20,7 +16,7 @@ namespace BeasiswaDesktop
             string username = textBox1.Text.Trim();
             string password = textBox2.Text.Trim();
 
-            // Validasi input kosong
+            
             if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
             {
                 MessageBox.Show("Username dan password tidak boleh kosong!",
@@ -30,46 +26,32 @@ namespace BeasiswaDesktop
 
             try
             {
-                conn.Open();
+                Admin admin = new Admin();
+                string[] adminData = admin.login(username, password);
 
-                string query = "SELECT IDAdmin, username FROM Admin WHERE username = @username AND password = @password";
-
-                using (SqlCommand cmd = new SqlCommand(query, conn))
+                if (adminData != null)
                 {
-                    cmd.Parameters.AddWithValue("@username", username);
-                    cmd.Parameters.AddWithValue("@password", password);
+                    string idAdmin = adminData[0];
+                    string namaAdmin = adminData[1];
 
-                    using (SqlDataReader reader = cmd.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            string idAdmin = reader["IDAdmin"].ToString();
-                            string namaAdmin = reader["username"].ToString();
+                    MessageBox.Show($"Selamat datang, {namaAdmin}!",
+                        "Login Berhasil", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-                            MessageBox.Show($"Selamat datang, {namaAdmin}!",
-                                "Login Berhasil", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-                            // Buka form utama & tutup form login
-                            MenuAdmin menuAdmin = new MenuAdmin(idAdmin, namaAdmin);
-                            menuAdmin.Show();
-                            this.Hide();
-                        }
-                        else
-                        {
-                            MessageBox.Show("Username atau password salah!",
-                                "Login Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        }
-                    }
+                    
+                    MenuAdmin menuAdmin = new MenuAdmin(idAdmin, namaAdmin);
+                    menuAdmin.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Username atau password salah!",
+                        "Login Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Terjadi kesalahan: " + ex.Message,
                     "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            finally
-            {
-                conn.Close();
             }
         }
 
