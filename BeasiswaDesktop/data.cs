4ccc3275b3b337ef54fc9b91134d766e906ae7cd
RizqiMaulana14 <maulanafitrah14@gmail.com>
2026-06-25 21:38:27 +0700

    class data

---
+++ BeasiswaDesktop/data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeasiswaDesktop
+{
+    internal class data
+    {
+        public string Nama { get; set; }
+        public string Jenjang { get; set; }
+        public string Kategori { get; set; }     
+        public DateTime TanggalInput { get; set; }
+    }
+}
